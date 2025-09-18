ce20488f1001facb2fa4d6c801039bfd45686963
JOE BRAYAN TCHINDA OUMBE <joeoumbe@gmail.com>
2025-09-18 22:29:08 +0200

    Add files via upload

---
+++ qr-code-generator/app.py
@@ -0,0 +1,38 @@
+import os
+import qrcode
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+# Dossier où les QR codes seront enregistrés
+qr_folder = os.path.join(app.static_folder, 'qr_codes')
+os.makedirs(qr_folder, exist_ok=True)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        data = request.form.get('data')
+        if data:
+            filename = f"{data.replace('https://', '').replace('/', '_')}.png"
+            filepath = os.path.join(qr_folder, filename)
+            generate_qr_code(data, filepath)
+            return redirect(url_for('index'))
+
+    qr_files = [f for f in os.listdir(qr_folder) if f.endswith('.png')]
+    return render_template('index.html', qr_files=qr_files)
+
+def generate_qr_code(data, filename):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(data)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save(filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+

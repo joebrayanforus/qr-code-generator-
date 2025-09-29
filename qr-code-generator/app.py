d5baecffcb63958a925c9f7dd5b8ee39d3999ece
JOE BRAYAN TCHINDA OUMBE <joeoumbe@gmail.com>
2025-09-30 00:36:05 +0200

    Update app.py

--- qr-code-generator/app.py
+++ qr-code-generator/app.py
@@ -1,12 +1,12 @@
+import os
+import io
 from flask import Flask, render_template, request, send_file
 from generate_qr_code import create_qr_code
-import io
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    qr_img = None
     if request.method == "POST":
         data = request.form.get("data")
         color = request.form.get("color", "black")
@@ -17,14 +17,21 @@ def index():
         # QR-Code erzeugen
         qr_img = create_qr_code(data, color, bgcolor, size, with_logo)
 
-        # Als Bytes speichern
+        # In Memory speichern
         img_bytes = io.BytesIO()
         qr_img.save(img_bytes, format="PNG")
         img_bytes.seek(0)
 
-        return send_file(img_bytes, mimetype="image/png", as_attachment=True, download_name="qr_code.png")
+        return send_file(
+            img_bytes,
+            mimetype="image/png",
+            as_attachment=True,
+            download_name="qr_code.png"
+        )
 
     return render_template("index.html")
-    
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
